0b900cf1606a6d5b7c89fc483546587c16d3b0c8
Giovanni Flores <geflores5@gmailcom>
2017-02-12 07:30:25 +0000

    Added New Book Page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,24 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(book_params)
+    if @book.invalid?
+      flash[:error] = '<strong>Could not save</strong> invalid data.'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre, :classification, :year)
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :classification, presence: true
+  validates :year, presence: true, numericality: true
 end
